60025c4cbfa2adad0c7fe398212fedc0cefab257
David Lowry <DavidLowry64@gmail.com>
2022-05-05 22:33:29 -0400

    Initial Commit HomeController

---
+++ ttrc/src/main/java/com/ttrc/home/TtrcApplication.java
@@ -0,0 +1,13 @@
+package com.ttrc.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtrcApplication.class, args);
+	}
+
+}
---
+++ ttrc/src/test/java/com/ttrc/home/TtrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttrc.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
